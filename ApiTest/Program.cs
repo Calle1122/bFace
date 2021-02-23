0b5749eff2dd7277e8853484ad2d676dcfc0d51d
Calle1122 <60339644+Calle1122@users.noreply.github.com>
2021-02-23 13:29:38 +0100

    nu kan spelaren välja pokemon!

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace ApiTest
 {
@@ -6,7 +8,24 @@ namespace ApiTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Please choose a pokemon");
+            string playerPokemon = Console.ReadLine();
+
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+            RestRequest request = new RestRequest("pokemon/" + playerPokemon);
+
+            IRestResponse response = client.Get(request);
+
+            //Console.WriteLine(response.Content);
+
+            Pokemon poke = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+
+            System.Console.WriteLine("Name: " + poke.name);
+            System.Console.WriteLine("Weight: " + poke.weight);
+            System.Console.WriteLine("Base EXP: " + poke.base_experience);
+
+            Console.ReadLine();
         }
     }
 }
